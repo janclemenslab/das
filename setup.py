330d3b4d32841b3cc6b32ae28ac2065486b82850
Jan Clemens <clemensjan@googlemail.com>
2020-01-22 12:45:32 +0100

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='deepsongsegmenter',
-      version='0.4',
+      version='0.5',
       description='deepsongsegmenter',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -17,8 +17,11 @@ setup(name='deepsongsegmenter',
       license='MIT',
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      install_requires=['numpy>=1.8.0', 'tensorflow>=2.0', 'h5py', 'scipy', 'sklearn', 'pyyaml', 'peakutils', 'zarr',
+      install_requires=['numpy>=1.8.0', 'h5py', 'scipy', 'sklearn', 'pyyaml', 'peakutils', 'zarr',
                         'flammkuchen', 'defopt', 'matplotlib', 'pandas', 'librosa', 'matplotlib', 'matplotlib_scalebar'],
+      extras_require = {"cpu": ["tensorflow>=2.0"],
+                        "gpu": ["tensorflow-gpu>=2.0"],
+                       },
       include_package_data=True,
       zip_safe=False
-      )
+     )
