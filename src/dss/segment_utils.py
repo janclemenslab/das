5c8604f7689259bd80a04a2043fcdbc480f6a944
Jan Clemens <clemensjan@googlemail.com>
2020-04-04 21:06:11 +0200

    added code for levenshtein edit distance

--- src/dss/segment_utils.py
+++ src/dss/segment_utils.py
@@ -2,58 +2,71 @@
 import numpy as np
 
 
-def levenshtein(source, target):
-    """Levenshstein (edit) distance
+# def levenshtein(source, target):
+#     """Levenshstein (edit) distance
 
-       Corresponds to the number of edits (deletions, insertions, or substitutions)
-       required to convert source string into target string.
+#        Corresponds to the number of edits (deletions, insertions, or substitutions)
+#        required to convert source string into target string.
 
-    Args:
-        source (str): in this context, predicted labels for songbird syllables
-        target (str): in this context, ground truth labels for songbird syllables
+#     Args:
+#         source (str): in this context, predicted labels for songbird syllables
+#         target (str): in this context, ground truth labels for songbird syllables
 
-    Returns:
-        [type]: Levenshtein distance
+#     Returns:
+#         [type]: Levenshtein distance
 
-    code copied from from https://github.com/NickleDave/vak/blob/master/src/vak/metrics.py
-    see from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
-    """
+#     code copied from from https://github.com/NickleDave/vak/blob/master/src/vak/metrics.py
+#     see from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
+#     """
 
-    if len(source) < len(target):
-        return levenshtein(target, source)
+#     if len(source) < len(target):
+#         return levenshtein(target, source)
 
-    # So now we have len(source) >= len(target).
-    if len(target) == 0:
-        return len(source)
+#     # So now we have len(source) >= len(target).
+#     if len(target) == 0:
+#         return len(source)
 
-    # We call tuple() to force strings to be used as sequences
-    # ('c', 'a', 't', 's') - numpy uses them as values by default.
-    source = np.array(tuple(source))
-    target = np.array(tuple(target))
+#     # We call tuple() to force strings to be used as sequences
+#     # ('c', 'a', 't', 's') - numpy uses them as values by default.
+#     source = np.array(tuple(source))
+#     target = np.array(tuple(target))
 
-    # We use a dynamic programming algorithm, but with the
-    # added optimization that we only need the last two rows
-    # of the matrix.
-    previous_row = np.arange(target.size + 1)
-    for s in source:
-        # Insertion (target grows longer than source):
-        current_row = previous_row + 1
+#     # We use a dynamic programming algorithm, but with the
+#     # added optimization that we only need the last two rows
+#     # of the matrix.
+#     previous_row = np.arange(target.size + 1)
+#     for s in source:
+#         # Insertion (target grows longer than source):
+#         current_row = previous_row + 1
 
-        # Substitution or matching:
-        # Target and source items are aligned, and either
-        # are different (cost of 1), or are the same (cost of 0).
-        current_row[1:] = np.minimum(
-                current_row[1:],
-                np.add(previous_row[:-1], target != s))
+#         # Substitution or matching:
+#         # Target and source items are aligned, and either
+#         # are different (cost of 1), or are the same (cost of 0).
+#         current_row[1:] = np.minimum(
+#                 current_row[1:],
+#                 np.add(previous_row[:-1], target != s))
 
-        # Deletion (target grows shorter than source):
-        current_row[1:] = np.minimum(
-                current_row[1:],
-                current_row[0:-1] + 1)
+#         # Deletion (target grows shorter than source):
+#         current_row[1:] = np.minimum(
+#                 current_row[1:],
+#                 current_row[0:-1] + 1)
 
-        previous_row = current_row
+#         previous_row = current_row
 
-    return previous_row[-1]
+#     return previous_row[-1]
+
+
+def levenshtein(seq1, seq2):
+    oneago = None
+    thisrow = list(range(1, len(seq2) + 1)) + [0]
+    for x in range(len(seq1)):
+        twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
+        for y in range(len(seq2)):
+            delcost = oneago[y] + 1
+            addcost = thisrow[y - 1] + 1
+            subcost = oneago[y - 1] + (seq1[x] != seq2[y])
+            thisrow[y] = min(delcost, addcost, subcost)
+    return thisrow[len(seq2) - 1]
 
 
 def syllable_error_rate(true, pred):
