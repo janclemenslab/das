f49cad718896f14d7268cbdad16e5bfd6427ee23
Jan Clemens <clemensjan@googlemail.com>
2021-06-18 11:49:12 +0200

    fix tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,19 +1,22 @@
 import pytest
 
 def test_import():
-    from dss import train
-    from dss import predict
-    from dss import evaluate
-    from dss import utils
-    from dss import utils_plot
-    from dss import data
-    from dss import io
-    from dss import npy_dir
-    from dss import models
-    from dss import pulse_utils
-    from dss import event_utils
-    from dss import segment_utils
-    import dss.kapre
-    import dss.tcn
+    from das import annot
+    from das import cli
+    from das import data
+    from das import evaluate
+    from das import event_utils
+    from das import io
+    from das import make_dataset
+    from das import models
+    from das import npy_dir
+    from das import predict
+    from das import pulse_utils
+    from das import segment_utils
+    from das import train
+    from das import utils
+    from das import utils_plot
+    import das.kapre
+    import das.tcn
 
 
