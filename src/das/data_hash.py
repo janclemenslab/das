ed35d18f0085edc4d5def55cb813e528b069723a
Jan Clemens <clemensjan@googlemail.com>
2021-07-30 22:08:23 +0200

    doc'ed and typ'ed

--- src/das/data_hash.py
+++ src/das/data_hash.py
@@ -1,8 +1,21 @@
 import hashlib
 from pathlib import Path
+from typing import Union
 
 
-def hash_data(data_path, chunk_size=65536):
+def hash_data(data_path: Union[str, Path], chunk_size: int = 65536):
+    """Compute MD5 hash of the data_path (dir or file) for data versioning.
+
+    Args:
+        data_path ([type]): [description]
+        chunk_size (int, optional): [description]. Defaults to 65536.
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        [type]: [description]
+    """
     if Path(data_path).is_dir():
         hash = _hash_dir(data_path, chunk_size)
     elif Path(data_path).is_file():
@@ -12,7 +25,8 @@ def hash_data(data_path, chunk_size=65536):
     return hash.hexdigest()
 
 
-def _update_hash_dir(directory, hash, chunk_size):
+def _update_hash_dir(directory: Union[str, Path], hash, chunk_size: int):
+    # from https://stackoverflow.com/questions/24937495/how-can-i-calculate-a-hash-for-a-filesystem-directory-using-python/54477583#54477583
     assert Path(directory).is_dir()
     for path in sorted(Path(directory).iterdir(), key=lambda p: str(p).lower()):
         hash.update(path.name.encode())
@@ -25,11 +39,11 @@ def _update_hash_dir(directory, hash, chunk_size):
     return hash
 
 
-def _hash_dir(directory, chunk_size):
+def _hash_dir(directory: Union[str, Path], chunk_size: int):
     return _update_hash_dir(directory, hashlib.md5(), chunk_size)
 
 
-def _hash_file(data_file, chunk_size):
+def _hash_file(data_file: Union[str, Path], chunk_size: int):
     hash = hashlib.md5()
     with open(data_file, "rb") as f:
         for chunk in iter(lambda: f.read(chunk_size), b""):
