bf73204c59f8969b18d3e846497d2e73f38fbd4d
Jan Clemens <clemensjan@googlemail.com>
2021-07-30 22:09:11 +0200

    added new modules

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,11 +4,13 @@ def test_import():
     from das import annot
     from das import cli
     from das import data
+    from das import data_hash
     from das import evaluate
     from das import event_utils
     from das import io
     from das import make_dataset
     from das import models
+    from das import neptune
     from das import npy_dir
     from das import predict
     from das import pulse_utils
