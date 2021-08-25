a5d971bcae1c89b57aa5e94123518c3a53ef8bba
Jan Clemens <clemensjan@googlemail.com>
2021-08-25 11:16:06 +0200

    added version command

--- src/das/cli.py
+++ src/das/cli.py
@@ -2,6 +2,40 @@ import defopt
 import logging
 from . import train, predict
 
+def version():
+    import das
+    import sys
+    import tensorflow.keras
+    import pandas as pd
+    import numpy as np
+    import scipy
+    import h5py
+    import sklearn as sk
+    import tensorflow as tf
+
+    try:
+        import xarray_behave.gui.app
+        has_gui = True
+    except (ImportError, ModuleNotFoundError):
+        has_gui = False
+
+    gpu = len(tf.config.list_physical_devices('GPU'))>0
+
+    print(f"The following versions are available to DAS:")
+    print(f"  DAS v{das.__version__}")
+    print("     GUI is", "available" if has_gui else "NOT AVAILABLE")
+    print("")
+    print(f"  tensorflow v{tf.__version__}")
+    print(f"  keras v{tensorflow.keras.__version__}")
+    print("     GPU is", "available" if gpu else "NOT AVAILABLE")
+    print("")
+    print(f"  python v{sys.version}")
+    print(f"  pandas v{pd.__version__}")
+    print(f"  numpy v{np.__version__}")
+    print(f"  h5py v{h5py.__version__}")
+    print(f"  scipy v{scipy.__version__}")
+    print(f"  scikit-learn v{sk.__version__}")
+
 
 def no_xb_gui():
     """Could not import the GUI. For instructions on how to install the GUI, check the docs janclemenslab.org/das/install.html."""
@@ -12,7 +46,7 @@ def no_xb_gui():
 
 def main():
     """Command line interface for DeepSS."""
-    subcommands = {'train': train.train, 'predict': predict.cli_predict}
+    subcommands = {'train': train.train, 'predict': predict.cli_predict, 'version': version}
 
     try:
         import xarray_behave.gui.app
