a4e197a4704b8f238591e5093cecdd3cdb72c431
Jan Clemens <clemensjan@googlemail.com>
2021-10-11 17:25:21 +0200

    add conda build files

---
+++ conda/das-nogui/build.sh
@@ -0,0 +1,8 @@
+# We need to turn pip index back on because Anaconda turns
+# it off for some reason.
+export PIP_NO_INDEX=False
+export PIP_NO_DEPENDENCIES=False
+export PIP_IGNORE_INSTALLED=False
+
+$PYTHON -m pip install das -vv
+
