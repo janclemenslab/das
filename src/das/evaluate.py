4d5a949f2dbca1dde691eb0907b13cad51cac099
Jan Clemens <clemensjan@googlemail.com>
2022-01-24 15:57:31 +0100

    re-format

--- src/das/evaluate.py
+++ src/das/evaluate.py
@@ -1,7 +1,7 @@
 import sklearn.metrics
 import numpy as np
 import pandas as pd
-from typing import Optional, Dict
+from typing import Optional, Dict, Callable
 import logging
 
 from . import predict, data, utils, models, io
@@ -9,8 +9,12 @@ from .event_utils import evaluate_eventtimes
 
 
 # to segment_utils
-def evaluate_segments(labels_test, labels_pred, class_names, confmat_as_pandas: bool = False,
-                      report_as_dict: bool = False, labels=None):
+def evaluate_segments(labels_test,
+                      labels_pred,
+                      class_names,
+                      confmat_as_pandas: bool = False,
+                      report_as_dict: bool = False,
+                      labels=None):
     """
 
     Args:
@@ -38,12 +42,17 @@ def evaluate_segments(labels_test, labels_pred, class_names, confmat_as_pandas: 
                                 index=['pred ' + p for p in class_names])
     if labels is None:
         labels = np.arange(len(class_names))
-    report = sklearn.metrics.classification_report(labels_test, labels_pred, labels=labels,
-                                                   target_names=class_names, output_dict=report_as_dict, digits=3)
+    report = sklearn.metrics.classification_report(labels_test,
+                                                   labels_pred,
+                                                   labels=labels,
+                                                   target_names=class_names,
+                                                   output_dict=report_as_dict,
+                                                   digits=3)
     return conf_mat, report
 
 
-def evaluate_segment_timing(segment_labels_true, segment_labels_pred, samplerate: float, event_tol: float):
+def evaluate_segment_timing(segment_labels_true, segment_labels_pred,
+                            samplerate: float, event_tol: float):
     """[summary]
 
     Args:
@@ -60,21 +69,29 @@ def evaluate_segment_timing(segment_labels_true, segment_labels_pred, samplerate
 
     # ensure evaluate_eventtimes returns nearest_predicted_onsets (nearest true event for each predicted event),
     # if not, rename var
-    segment_onsets_report, _, _, nearest_predicted_onsets = evaluate_eventtimes(segment_onsets_true, segment_onsets_pred, samplerate, event_tol)
-    segment_offsets_report, _, _, nearest_predicted_offsets = evaluate_eventtimes(segment_offsets_true, segment_offsets_pred, samplerate, event_tol)
+    segment_onsets_report, _, _, nearest_predicted_onsets = evaluate_eventtimes(
+        segment_onsets_true, segment_onsets_pred, samplerate, event_tol)
+    segment_offsets_report, _, _, nearest_predicted_offsets = evaluate_eventtimes(
+        segment_offsets_true, segment_offsets_pred, samplerate, event_tol)
     return segment_onsets_report, segment_offsets_report, nearest_predicted_onsets, nearest_predicted_offsets
 
 
 # TODO: move to das.segment_utils
 def segment_timing(labels, samplerate: float):
     """Get onset and offset time (in seconds) for each segment."""
-    segment_onset_times = np.where(np.diff(labels) == 1)[0].astype(np.float) / samplerate  # explicit cast required?
-    segment_offset_times = np.where(np.diff(labels) == -1)[0].astype(np.float) / samplerate
+    segment_onset_times = np.where(np.diff(labels) == 1)[0].astype(
+        np.float) / samplerate  # explicit cast required?
+    segment_offset_times = np.where(np.diff(labels) == -1)[0].astype(
+        np.float) / samplerate
     return segment_onset_times, segment_offset_times
 
 
-# def evaluate_probabilities(x, y, model, params, verbose=None):
-def evaluate_probabilities(x, y, model: Optional[models.keras.models.Model] = None, params: Optional[Dict] = None, model_savename: Optional[str] = None, verbose: int = 1):
+def evaluate_probabilities(x,
+                           y,
+                           model: Optional[models.keras.models.Model] = None,
+                           params: Optional[Dict] = None,
+                           model_savename: Optional[str] = None,
+                           verbose: int = 1):
     """[summary]
 
     evaluate_probabilities(x, y, model=keras_model, params=params_dict)
@@ -91,15 +108,21 @@ def evaluate_probabilities(x, y, model: Optional[models.keras.models.Model] = No
     Returns:
         [type]: [description]
     """
-
+    # TODO if called w/o x and y, load dataset from params
     if model is None or params is None:
         if model_savename is not None:
             model, params = utils.load_model_and_params(model_savename)
         else:
-            raise ValueError(f'Required: Either a model and params OR a model_savename so we can load model and params. But model={model}, params={params}, model_savename={model_savename}.')
+            raise ValueError(
+                f'Required: Either a model and params OR a model_savename so we can load model and params. But model={model}, params={params}, model_savename={model_savename}.'
+            )
 
     # do not prepend padding since we create y from the generator
-    y_pred = predict.predict_probabililties(x, model, params, verbose, prepend_data_padding=False)
+    y_pred = predict.predict_probabililties(x,
+                                            model=model,
+                                            params=params,
+                                            verbose=verbose,
+                                            prepend_data_padding=False)
 
     eval_gen = data.AudioSequence(x, y, shuffle=False, **params)
     x, y = data.get_data_from_gen(eval_gen)
