5e89ebfced769b4f26950b3517ef78f09c673f6b
Jan Clemens <clemensjan@googlemail.com>
2022-04-23 17:48:21 +0200

    osx-arm build files

--- conda/das-osxarm/build.sh
+++ conda/das-osxarm/build.sh
@@ -4,6 +4,7 @@ export PIP_NO_INDEX=False
 export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
+$PYTHON -m pip install keras-tuner -vv --no-dependencies
 $PYTHON -m pip install kt-legacy -vv --no-dependencies
 $PYTHON -m pip install tensorflow-macos
 $PYTHON -m pip install tensorflow-metal
