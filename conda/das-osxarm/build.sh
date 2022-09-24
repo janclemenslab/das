78254d813f8b7a7729d5fbab1282199264694cda
Jan Clemens <clemensjan@googlemail.com>
2022-09-24 09:22:41 +0200

    simpler/faster builds

--- conda/das-osxarm/build.sh
+++ conda/das-osxarm/build.sh
@@ -4,8 +4,8 @@ export PIP_NO_INDEX=False
 export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
-$PYTHON -m pip install keras-tuner -vv --no-dependencies
-$PYTHON -m pip install kt-legacy -vv --no-dependencies
-$PYTHON -m pip install tensorflow-macos tensorflow-metal
+$PYTHON -m pip install keras-tuner==1.1.2 kt-legacy -vv --no-dependencies
+# $PYTHON -m pip install kt-legacy -vv --no-dependencies
+$PYTHON -m pip install tensorflow-macos==2.8.0 tensorflow-metal=0.5.0
 $PYTHON -m pip install das -vv --no-dependencies
 
