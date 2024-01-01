49adc577eb927829c723ecab97654b9a0d8b8bea
Jan Clemens <clemensjan@googlemail.com>
2024-01-01 22:11:10 +0100

    osx

--- conda/das-osxarm/build.sh
+++ conda/das-osxarm/build.sh
@@ -7,6 +7,7 @@ export PIP_IGNORE_INSTALLED=False
 $PYTHON -m pip install keras-tuner==1.1.2 kt-legacy -vv --no-dependencies
 # $PYTHON -m pip install kt-legacy -vv --no-dependencies
 # $PYTHON -m pip install tensorflow-macos==2.9 tensorflow-metal==0.5.0
-$PYTHON -m pip install tensorflow-macos==2.13 tensorflow-metal==1.0.0
+# $PYTHON -m pip install tensorflow-macos==2.13 tensorflow-metal==1.0.0
+$PYTHON -m pip install tensorflow-macos==2.14 tensorflow-metal==1.1.0
 $PYTHON -m pip install das -vv --no-dependencies
 
