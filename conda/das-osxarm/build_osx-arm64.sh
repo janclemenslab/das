120554d52290cbdc4b358f05d5ec2acfa1282428
Jan Clemens <clemensjan@googlemail.com>
2024-01-13 19:25:12 +0100

    test

---
+++ conda/das-osxarm/build_osx-arm64.sh
@@ -0,0 +1,13 @@
+# We need to turn pip index back on because Anaconda turns
+# it off for some reason.
+export PIP_NO_INDEX=False
+export PIP_NO_DEPENDENCIES=False
+export PIP_IGNORE_INSTALLED=False
+
+$PYTHON -m pip install keras-tuner==1.1.2 kt-legacy -vv --no-dependencies
+# $PYTHON -m pip install kt-legacy -vv --no-dependencies
+# $PYTHON -m pip install tensorflow-macos==2.9 tensorflow-metal==0.5.0
+# $PYTHON -m pip install tensorflow-macos==2.13 tensorflow-metal==1.0.0
+$PYTHON -m pip install tensorflow-macos==2.14 tensorflow-metal==1.1.0
+$PYTHON -m pip install das -vv --no-dependencies
+
