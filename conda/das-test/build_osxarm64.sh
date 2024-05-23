4d205780814893712068e1b3d9635973b75bc1e9
Jan Clemens <clemensjan@googlemail.com>
2024-05-23 17:14:20 -0400

    Update build_osxarm64.sh

--- conda/das-test/build_osxarm64.sh
+++ conda/das-test/build_osxarm64.sh
@@ -5,6 +5,6 @@ export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
 $PYTHON -m pip install keras-tuner==1.1.2 kt-legacy -vv --no-dependencies
-$PYTHON -m pip install tensorflow-macos==2.14 tensorflow-metal==1.1.0
+$PYTHON -m pip install tensorflow-macos==2.16 tensorflow-metal==1.1.0
 $PYTHON -m pip install das -vv --no-dependencies
 
