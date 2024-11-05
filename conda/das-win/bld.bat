3f6d9b464d089ca77e278b951d901c502a6b7b5a
Jan Clemens <clemensjan@googlemail.com>
2024-11-05 12:54:41 +0100

    dummy v0.32.4 to test new builds

--- conda/das-win/bld.bat
+++ conda/das-win/bld.bat
@@ -2,5 +2,5 @@ set PIP_NO_INDEX=False
 set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
 @REM pip install "tensorflow==2.10.*"
-pip install das==0.32.3 --no-dependencies
+pip install das --no-dependencies
 if errorlevel 1 exit 1
